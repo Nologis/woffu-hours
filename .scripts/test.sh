161bd6f16700e632cafc5be4998cbd8b0646de53
Richard Morales <richmorales@nologis.com>
2023-06-20 10:27:18 +0100

    Fix script test.sh

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -2,10 +2,8 @@ WEBHOOK_URL="${WEBHOOK_URL}"
 MESSAGE="${MESSAGE}"
 if [ $1=='start' ]; then
     curl -X POST -H 'Content-type: application/json' --data '{"text":'${MESSAGE}'}' ${WEBHOOK_URL}
-         ok%
 else
     curl -X POST -H 'Content-type: application/json' --data '{"text":'${MESSAGE}'}' ${WEBHOOK_URL}
-         ok%
 fi
 
 
