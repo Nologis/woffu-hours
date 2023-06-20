7bbcee922e8eacbe361d958993a4e381d31ea688
Richard Morales <richmorales@nologis.com>
2023-06-20 11:41:07 +0100

    test slack message

--- .scripts/test.sh
+++ .scripts/test.sh
@@ -1,7 +1,7 @@
 WEBHOOK_URL="${WEBHOOK_URL}"
 MESSAGE="${MESSAGE}"
 
-echo WEBHOOK_URL
+echo ${WEBHOOK_URL}
 if [ $1=='start' ]; then
     curl -X POST -H 'Content-type: application/json' --data '{"text":'${MESSAGE}'}' ${WEBHOOK_URL}
 else
