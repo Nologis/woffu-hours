f43822fc495ab9246a24e0208c4cd5d46b77cbad
Richard Morales <richmorales@nologis.com>
2023-11-07 13:43:22 +0000

    remove stricts engines

--- tests/fillHours.spec.ts
+++ tests/fillHours.spec.ts
@@ -1,6 +1,6 @@
-import { config } from "dotenv";
+import { config } from 'dotenv';
 import { Page, test } from '@playwright/test';
-import { fillHours, goToReport, woffuURL } from "./utils";
+import { fillHours, goToReport, woffuURL } from './utils';
 
 config();
 
@@ -10,59 +10,59 @@ const nextButton = { name: 'Siguiente' };
 const initSessionButton = { name: 'Iniciar sesión' };
 
 const linkGoogle = { name: 'Entrar con Google' };
-const emailTextBox = {name: 'Correo electrónico o teléfono'};
+const emailTextBox = { name: 'Correo electrónico o teléfono' };
 const passwordTextBox = { name: 'Introduce tu contraseña' };
 
 const buildSetup = (page: Page) => ({
-    doLogin: async () => {
-        await page.goto(`${woffuURL}/V2/login`);
-        await page.getByPlaceholder(emailInput).click();
-        await page.getByPlaceholder(emailInput).fill(process.env.EMAIL);
-        await page.getByRole('button', nextButton).click();
-        await page.getByPlaceholder(passwordInput).click();
-        await page.getByPlaceholder(passwordInput).fill(process.env.PASSWORD);
+  doLogin: async () => {
+    await page.goto(`${woffuURL}/V2/login`);
+    await page.getByPlaceholder(emailInput).click();
+    await page.getByPlaceholder(emailInput).fill(process.env.EMAIL);
+    await page.getByRole('button', nextButton).click();
+    await page.getByPlaceholder(passwordInput).click();
+    await page.getByPlaceholder(passwordInput).fill(process.env.PASSWORD);
 
-        await Promise.all([
-            page.waitForNavigation(),
-            page.getByRole('button', initSessionButton).click()
-        ]);
-    },
-    doLoginWithGoogle: async () => {
-        await page.goto(`${woffuURL}/V2/login`);
-        await Promise.all([
-            page.waitForNavigation(),
-            page.getByRole('link', linkGoogle).click()
-        ]);
+    await Promise.all([
+      page.waitForNavigation(),
+      page.getByRole('button', initSessionButton).click()
+    ]);
+  },
+  doLoginWithGoogle: async () => {
+    await page.goto(`${woffuURL}/V2/login`);
+    await Promise.all([
+      page.waitForNavigation(),
+      page.getByRole('link', linkGoogle).click()
+    ]);
 
-        await page.getByRole('textbox', emailTextBox).click();
-        await page.getByRole('textbox', emailTextBox).fill(process.env.EMAIL);
-        await page.getByRole('button', nextButton).click();
+    await page.getByRole('textbox', emailTextBox).click();
+    await page.getByRole('textbox', emailTextBox).fill(process.env.EMAIL);
+    await page.getByRole('button', nextButton).click();
 
-        await page.getByRole('textbox', passwordTextBox).click();
-        await page.getByRole('textbox', passwordTextBox).fill(process.env.PASSWORD);
-        await page.getByRole('button', nextButton).click();
-    }
+    await page.getByRole('textbox', passwordTextBox).click();
+    await page.getByRole('textbox', passwordTextBox).fill(process.env.PASSWORD);
+    await page.getByRole('button', nextButton).click();
+  }
 });
 
 test('fill hours of previous month in Woffu', async ({ page }) => {
-    if (process.env.HAS_GOOGLE_LOGIN === 'true') {
-        const { doLoginWithGoogle } = buildSetup(page);
-        await doLoginWithGoogle();
-    } else {
-        const { doLogin } = buildSetup(page);
-        await doLogin();
-    }
-    console.info('🔐 Logged in');
-    await goToReport(page);
+  if (process.env.HAS_GOOGLE_LOGIN === 'true') {
+    const { doLoginWithGoogle } = buildSetup(page);
+    await doLoginWithGoogle();
+  } else {
+    const { doLogin } = buildSetup(page);
+    await doLogin();
+  }
+  console.info('🔐 Logged in');
+  await goToReport(page);
 
-    const totalMonth: number = parseInt(process.env.TOTAL_MONTH);
-    for (let index = 1; index < totalMonth; index++) {
-        await page.frameLocator('#woffu-legacy-calendar').locator('text=< mes anterior').click();
-    }
+  const totalMonth: number = parseInt(process.env.TOTAL_MONTH);
+  for (let index = 1; index < totalMonth; index++) {
+    await page.frameLocator('#woffu-legacy-calendar').locator('text=< mes anterior').click();
+  }
 
-    for (let index = 1; index <= totalMonth; index++) {
-        console.info(`Filling hours. (${index}/${totalMonth})`);
-        await fillHours(page);
-        await page.frameLocator('#woffu-legacy-calendar').locator('text=mes siguiente >').click();
-    }
+  for (let index = 1; index <= totalMonth; index++) {
+    console.info(`Filling hours. (${index}/${totalMonth})`);
+    await fillHours(page);
+    await page.frameLocator('#woffu-legacy-calendar').locator('text=mes siguiente >').click();
+  }
 });
